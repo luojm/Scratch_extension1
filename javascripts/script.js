b6bae603802800bf4d61f38312f62b1335ff8b32
luojm <raku0525@gmail.com>
2016-06-03 11:53:57 +0900

    test

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,52 +1,24 @@
-(function($) {
-$(document).ready(function(){
+(function(ext) {
+    // shutdown時に呼ばれる
+    ext._shutdown = function() {};
 
-  // putting lines by the pre blocks
-  $("pre").each(function(){
-    var pre = $(this).text().split("\n");
-    var lines = new Array(pre.length+1);
-    for(var i = 0; i < pre.length; i++) {
-      var wrap = Math.floor(pre[i].split("").length / 70)
-      if (pre[i]==""&&i==pre.length-1) {
-        lines.splice(i, 1);
-      } else {
-        lines[i] = i+1;
-        for(var j = 0; j < wrap; j++) {
-          lines[i] += "\n";
-        }
-      }
-    }
-    $(this).before("<pre class='lines'>" + lines.join("\n") + "</pre>");
-  });
+    // statusを返してやる。デバイスとつながってない時とかここで色々返せる。
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
 
-  var headings = [];
+    // blockが呼び出された時に呼ばれる関数を登録する。
+    // 下にあるdescriptorでブロックと関数のひも付けを行っている。
+    ext.do_something = function(str) {
+    };
 
-  var collectHeaders = function(){
-    headings.push({"top":$(this).offset().top - 15,"text":$(this).text()});
-  }
+    // ブロックと関数のひも付け
+    var descriptor = {
+        blocks: [
+            [' ', 'do_something %s', 'do_something', 'sample text'],
+        ]
+    };
 
-  if($(".markdown-body h1").length > 1) $(".markdown-body h1").each(collectHeaders)
-  else if($(".markdown-body h2").length > 1) $(".markdown-body h2").each(collectHeaders)
-  else if($(".markdown-body h3").length > 1) $(".markdown-body h3").each(collectHeaders)
-
-  $(window).scroll(function(){
-    if(headings.length==0) return true;
-    var scrolltop = $(window).scrollTop() || 0;
-    if(headings[0] && scrolltop < headings[0].top) {
-      $(".current-section").css({"opacity":0,"visibility":"hidden"});
-      return false;
-    }
-    $(".current-section").css({"opacity":1,"visibility":"visible"});
-    for(var i in headings) {
-      if(scrolltop >= headings[i].top) {
-        $(".current-section .name").text(headings[i].text);
-      }
-    }
-  });
-
-  $(".current-section a").click(function(){
-    $(window).scrollTop(0);
-    return false;
-  })
-});
-})(jQuery)
+    // 最後にExtensionを登録する
+    ScratchExtensions.register('Simple extension', descriptor, ext);
+})({});
